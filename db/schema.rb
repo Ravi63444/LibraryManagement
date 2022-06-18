bc5375483d1ed208dd95e48960ae2de5cbd45420
ravi jyala <ravi63444@gmail.com>
2022-06-18 17:08:21 +0530

    created users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_163607) do
+ActiveRecord::Schema.define(version: 2022_06_18_112912) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
     t.text "Author"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "readers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
